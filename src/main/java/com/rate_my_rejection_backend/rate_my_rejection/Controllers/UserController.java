3e347148199b9f19b384a617ba792e923125e6a3
ChrisPrice03 <squacknog@gmail.com>
2025-02-22 15:13:05 -0500

    Updated User stats

--- src/main/java/com/rate_my_rejection_backend/rate_my_rejection/Controllers/UserController.java
+++ src/main/java/com/rate_my_rejection_backend/rate_my_rejection/Controllers/UserController.java
@@ -24,7 +24,40 @@ public class UserController {
     public Map<String, Object> getUserReviews(@PathVariable String username) {
         List<Review> reviews = reviewService.getUserReviews(username);
 
+        double totalConfidence = 0;
+        double totalCompetitiveness = 0;
+        int totalNumRounds = 0;
+        int applyYes = 0;
+        int applyNo = 0;
+        Map<String, Long> rejectTypeCount = new HashMap<>();
+
+        for (Review review : reviews) {
+            totalConfidence += review.getConfidence();
+            totalCompetitiveness += review.getCompetitiveness();
+            totalNumRounds += review.getNumRounds();
+            applyYes += review.getApplyAgain() ? 1 : 0;
+            applyNo += review.getApplyAgain() ? 0 : 1;
+
+            rejectTypeCount.put(review.getSelected(), rejectTypeCount.getOrDefault(review.getSelected(), 0L) + 1);
+        }
+
+        int reviewCount = reviews.size();
+        double confidence = reviewCount > 0 ? totalConfidence / reviewCount : 0;
+        double competitiveness = reviewCount > 0 ? totalCompetitiveness / reviewCount : 0;
+        double numRounds = reviewCount > 0 ? (double) totalNumRounds / reviewCount : 0;
+        String rejectType = rejectTypeCount.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+
         Map<String, Object> response = new HashMap<>();
+
+        response.put("confidence", confidence);
+        response.put("competitveness", competitiveness);
+        response.put("rejectType", rejectType);
+        response.put("numRounds", numRounds);
+        response.put("applyYes", applyYes);
+        response.put("applyNo", applyNo);
         response.put("reviews", reviews);
 
         return response;
