f52eb8b3c1204927e617273caaef58a6c34c9157
Mary Voorhees <mvoorhe@purdue.edu>
2025-02-22 01:41:46 -0500

    merge didn't work out

--- src/main/java/com/rate_my_rejection_backend/rate_my_rejection/dto/Company.java
+++ src/main/java/com/rate_my_rejection_backend/rate_my_rejection/dto/Company.java
@@ -2,8 +2,15 @@ package com.rate_my_rejection_backend.rate_my_rejection.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Company {
 
+    @Id
+    private Long id;
+    
     @JsonProperty("name")
     private String name;
 
