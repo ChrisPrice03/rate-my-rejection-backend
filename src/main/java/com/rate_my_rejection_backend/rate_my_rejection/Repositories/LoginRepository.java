059c8bb3e6e13025225178b24088f8c546cd4eb5
ChrisPrice03 <squacknog@gmail.com>
2025-02-22 10:38:28 -0500

    Login creation and validation

---
+++ src/main/java/com/rate_my_rejection_backend/rate_my_rejection/Repositories/LoginRepository.java
@@ -0,0 +1,10 @@
+package com.rate_my_rejection_backend.rate_my_rejection.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.rate_my_rejection_backend.rate_my_rejection.dto.Login;
+import java.util.Optional;
+
+
+public interface LoginRepository extends JpaRepository<Login, Long> {
+    boolean existsByUsernameAndPassword(String username, String password);
+}
---
+++ src/main/java/com/rate_my_rejection_backend/rate_my_rejection/Service/LoginService.java
@@ -0,0 +1,22 @@
+package com.rate_my_rejection_backend.rate_my_rejection.service;
+
+import com.rate_my_rejection_backend.rate_my_rejection.dto.Login;
+import com.rate_my_rejection_backend.rate_my_rejection.Repositories.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private LoginRepository loginRepository;
+
+    public boolean validateCredentials(String username, String password) {
+        return loginRepository.existsByUsernameAndPassword(username, password);
+    }
+
+    public Login createUser(String username, String password) {
+        Login newUser = new Login(username, password);
+        return loginRepository.save(newUser);
+    }
+}
