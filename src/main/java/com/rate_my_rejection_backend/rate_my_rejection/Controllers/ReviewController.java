b11808dc925a22fa279e83b9a1489463189c84bb
ChrisPrice03 <squacknog@gmail.com>
2025-02-22 00:32:55 -0500

    Test in reviewController

--- src/main/java/com/rate_my_rejection_backend/rate_my_rejection/Controllers/ReviewController.java
+++ src/main/java/com/rate_my_rejection_backend/rate_my_rejection/Controllers/ReviewController.java
@@ -3,6 +3,8 @@ package com.rate_my_rejection_backend.rate_my_rejection.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/company")
@@ -10,6 +12,39 @@ public class ReviewController {
 
     @GetMapping("/{companyName}")  // Dynamic path variable
     public String getCompanyInfo(@PathVariable String companyName) {
-        return "company name";
+        return "[\n" +
+                "  {\n" +
+                "  \"name\": \"companyName\",\n" +
+                "  \"rating\": 5,\n" +
+                "  \"competitiveness\": 5,\n" +
+                "  \"applyAgain\": 50\n" +
+                "  },\n" +
+                "  [\n" +
+                "    {\n" +
+                "      \"role\" : \"rolestr\",\n" +
+                "      \"quality\" : 3,\n" +
+                "      \"confidence\" : 3,\n" +
+                "      \"competitiveness\": 3,\n" +
+                "      \"selected\": \"selectStr\",\n" +
+                "      \"numRounds\": 3\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"role\" : \"rolestr2\",\n" +
+                "      \"quality\" : 3,\n" +
+                "      \"confidence\" : 4,\n" +
+                "      \"competitiveness\": 3,\n" +
+                "      \"selected\": \"selectStr\",\n" +
+                "      \"numRounds\": 2\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"role\" : \"rolestr3\",\n" +
+                "      \"quality\" : 8,\n" +
+                "      \"confidence\" : 6,\n" +
+                "      \"competitiveness\": 1,\n" +
+                "      \"selected\": \"selectStr\",\n" +
+                "      \"numRounds\": 2\n" +
+                "    }\n" +
+                "  ]\n" +
+                "]";
     }
 }
